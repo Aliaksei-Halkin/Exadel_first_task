0bd9bbdab52a9e9171b2a0f4164eadf070b4158b
Aliaksei Halkin <pochtax3@gmail.com>
2021-03-23 16:48:09 +0300

    'time'

---
+++ time_with_spring/src/main/java/com/example/time/GreetingController.java
@@ -0,0 +1,21 @@
+package com.example.time;
+
+import org.joda.time.LocalTime;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        LocalTime currentTime = LocalTime.now();
+        model.put("some", currentTime);
+        return "main";
+    }
+
+}
